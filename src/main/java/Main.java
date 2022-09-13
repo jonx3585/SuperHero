414b94224b3536825d79e41d88972223a8cdf976
jonx3585 <jonx3585@stud.kea.dk>
2022-09-13 10:58:00 +0200

    basic Main Class made

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,38 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int answer;
         Scanner sc = new Scanner(System.in);
         DataBase db = new DataBase();
 
-        System.out.println("Velkommen til superhelte Databasen");
+        System.out.println("Velkommen til SUPERHERO UNIVERSET \n 1. Opret superhelt \n 9. Afslut ");
+        answer = sc.nextInt();
+        if(answer != 9) {
 
-        System.out.println("Indtast årstal for skabelse af helt");
-        int creationYear = sc.nextInt();
+            System.out.println("Indtast årstal for skabelse af helt");
+            int creationYear = sc.nextInt();
 
-        System.out.println("Indtast borgerligt navn for helt");
-        String citizenName = sc.nextLine();
+            System.out.println("Indtast borgerligt navn for helt");
+            String citizenName = sc.nextLine();
 
-        System.out.println("Indtast superhelte navn for helt");
-        String superHeroName = sc.nextLine();
+            System.out.println("Indtast superhelte navn for helt");
+            String superHeroName = sc.nextLine();
 
-        System.out.println("Indtast heltens superkraft");
-        String power = sc.nextLine();
+            System.out.println("Indtast heltens superkraft");
+            String power = sc.nextLine();
 
-        System.out.println("Indtast styrkeniveau for helt");
-        double strength = sc.nextDouble();
+            System.out.println("Indtast styrkeniveau for helt");
+            double strength = sc.nextDouble();
 
-        System.out.println("Indtast om helt er menneske");
-        boolean isHuman = sc.nextBoolean();
+            System.out.println("Indtast om helt er menneske");
+            boolean isHuman = sc.nextBoolean();
 
-        db.makeHero(creationYear, citizenName, superHeroName, power, strength, isHuman);
+            db.makeHero(creationYear, citizenName, superHeroName, power, strength, isHuman);
 
+        }
+        else {
+            System.out.println("Goodbye");
+        }
 
     }
 
