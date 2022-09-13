62368223f8b7e9f2e113947841f26ca347a85e24
jonx3585 <jonx3585@stud.kea.dk>
2022-09-13 09:52:20 +0200

    basic DataBase Class made

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,3 +1,22 @@
 public class DataBase {
+    private SuperHero[] heroes = new SuperHero[5];
+    private int numOfHeroes;
+    public DataBase()
+    {
 
+    }
+
+    public void makeHero(int creationYear, String citizenName,String superHeroName, String power, double strength, boolean isHuman){
+
+        SuperHero hero = new SuperHero();
+        hero.setCreationYear(creationYear);
+        hero.setCitizenName(citizenName);
+        hero.setSuperHeroName(superHeroName);
+        hero.setPower(power);
+        hero.setStrength(strength);
+        hero.setIsHuman(isHuman);
+
+        heroes[numOfHeroes++] = hero;
+
+    }
 }
--- src/main/java/SuperHero.java
+++ src/main/java/SuperHero.java
@@ -1,35 +1,72 @@
 public class SuperHero {
     private int creationYear;
-    private String name;
+    private String citizenName;
+    private String superHeroName;
     private String power;
+    private double strength;
+    boolean isHuman = true;
 
-    public SuperHero(int creationYear, String name, String power)
-    {
+    public SuperHero(int creationYear, String citizenName, String superHeroName, String power, double strength, boolean isHuman) {
         this.creationYear = creationYear;
-        this.name = name;
+        this.citizenName = citizenName;
+        this.superHeroName = superHeroName;
         this.power = power;
+        this.strength = strength;
+        this.isHuman = isHuman;
+    }
 
+    public SuperHero() {
+
+    }
+
+    public double getStrength() {
+        return strength;
     }
 
     public int getCreationYear() {
         return creationYear;
     }
-    public String getName() {
-        return name;
+    public String getCitizenName() {
+        return citizenName;
     }
+
+    public String getSuperHeroName() {
+        return superHeroName;
+    }
+
     private String getPower(){
         return power;
+    }
+    private boolean getIsHuman()
+    {
+        return isHuman;
+    }
+
+    public void setStrength(double strength) {
+        this.strength = strength;
     }
 
     public void setCreationYear(int creationYear) {
         this.creationYear = creationYear;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String citizenName) {
+        this.citizenName = citizenName;
+    }
+
+    public void setSuperHeroName(String superHeroName) {
+        this.superHeroName = superHeroName;
+    }
+
+    public void setCitizenName(String citizenName) {
+        this.citizenName = citizenName;
     }
 
     public void setPower(String power) {
         this.power = power;
+    }
+
+    public void setIsHuman(boolean isHuman) {
+        isHuman = isHuman;
     }
 }
